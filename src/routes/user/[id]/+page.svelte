9039f04732cb66c8ada954ef9fb18e7ee1faaa01
Adam Thoor <adam.thoor@appbolaget.se>
2022-12-18 15:53:26 +0100

    Sign out

--- src/routes/user/[id]/+page.svelte
+++ src/routes/user/[id]/+page.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-    import type { PageData } from "./$types";
+    import Button from '$lib/components/Button.svelte';
+    import { signOut } from '@auth/sveltekit/client';
+    import type { PageData } from './$types';
 
     export let data: PageData;
 </script>
 
 {#if data.user}
-<h2 class="text-2xl font-medium">{data.user.name}</h2>
+    <h2 class="text-2xl font-medium">{data.user.name}</h2>
 
-<div class="flex flex-row gap-1">
-    <dt>Karma:</dt>
-    <dd>{data.user._count.votes}</dd>
-</div>
+    <div class="flex flex-row gap-1">
+        <dt>Karma:</dt>
+        <dd>{data.user._count.votes}</dd>
+    </div>
 
-<div class="flex flex-row gap-1">
-    <dt>Posts:</dt>
-    <dd>{data.user._count.articles}</dd>
-</div>
+    <div class="flex flex-row gap-1">
+        <dt>Posts:</dt>
+        <dd>{data.user._count.articles}</dd>
+    </div>
+
+    {#if data.user.id === data.session?.user.id}
+        <Button on:click={() => signOut({ callbackUrl: '/' })}>Sign out</Button>
+    {/if}
 {:else}
-<h2>User not found</h2>
+    <h2>User not found</h2>
 {/if}
